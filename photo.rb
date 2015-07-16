4aabf7c2e6eebd6cc862fdde5485dd4b4caab948
Nicholas Emanuel <njemanuel01@gmail.com>
2015-07-16 11:29:51 -0500

    Added top_photo? method to photo model

--- photo.rb
+++ photo.rb
@@ -2,4 +2,9 @@ class Photo < ActiveRecord::Base
   has_and_belongs_to_many :albums
   validates :name, :photographer_id, presence: true
   validates :photogrpaher_id, numericality: true
+  
+  def top_photo?
+    self.albums.length > 2
+  end
 end
+
