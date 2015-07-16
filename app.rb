05f5fd4be823d75c474435981bb65d5c1772702a
Nicholas Emanuel <njemanuel01@gmail.com>
2015-07-16 10:34:39 -0500

    Database setup in app.rb

--- app.rb
+++ app.rb
@@ -10,4 +10,28 @@ require_relative "photographer"
 require_relative "photo"
 require_relative "album"
 
-binding.pry
+unless ActiveRecord::Base.connection.table_exists?(:photographers)
+  ActiveRecord::Base.connection.create_table :photographers do |t|
+    t.text :name
+  end  
+end
+
+unless ActiveRecord::Base.connection.table_exists?(:photos)
+  ActiveRecord::Base.connection.create_table :photos do |t|
+    t.text :name
+    t.integer :photographer_id
+  end  
+end
+
+unless ActiveRecord::Base.connection.table_exists?(:albums)
+  ActiveRecord::Base.connection.create_table :albums do |t|
+    t.text :name
+  end  
+end
+
+unless ActiveRecord::Base.connection.table_exists?(:albums_photos)
+  ActiveRecord::Base.connection.create_table :albums_photos do |t|
+    t.integer :album_id
+    t.integer :photo_id
+  end  
+end
